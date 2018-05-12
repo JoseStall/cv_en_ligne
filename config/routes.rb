e9763cf35c77bf06c19638c6be73b8775d09f39c
guillaume <g.delory62@gmail.com>
2018-05-12 14:57:16 +0200

    page d'accueil

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/show'
+  get 'contact', to: 'home#contact', as: 'contact'
+  get 'realisations', to: 'home#show', as: 'realisations'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
